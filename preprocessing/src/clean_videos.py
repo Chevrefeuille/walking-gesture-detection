de5728cefb09b9ae80ca6d469dce4d2f2358f231
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2018-03-01 10:41:29 +0900

    also delete non classified videos

--- preprocessing/src/clean_videos.py
+++ preprocessing/src/clean_videos.py
@@ -6,6 +6,18 @@ if __name__ == "__main__":
     p = Path('videos/dyads/').glob('**/*.avi')
     for path in p: # each video folder
         video_path = str(path)
+        print('Removing ' + video_path)
         os.remove(video_path)
+
+    folder = '2010_10_06'
+    p = Path('videos/' + folder)
+    for path in p.iterdir(): # each video folder
+        if path.is_dir():
+            dyads_folder = str(path) 
+            dp = Path(dyads_folder)
+            for v_path in dp.glob('dyads/*.avi'): # each subvideo
+                video_path = str(v_path)
+                print('Removing ' + video_path)
+                os.remove(video_path)
                         
 
