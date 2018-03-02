70961b2b477ed687cd29de48d246f6e8a47d0572
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2018-03-02 14:34:17 +0900

    fix wrong refactoring issue

--- preprocessing/src/calibration.py
+++ preprocessing/src/calibration.py
@@ -12,34 +12,24 @@ def plot_coords(event, x, y, flags, param):
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255)
         )
 
-def input_reference():
-    """
-    Open a video and wait for the user to select references point.
-    """
-    dyad_cap = cv2.VideoCapture('./videos/2010_10_06/00000/00000.avi')
 
-    # read first frame
-    ret, frame = dyad_cap.read()
+dyad_cap = cv2.VideoCapture('./videos/2010_10_06/00001/00001.avi')
 
-    frame = cv2.resize(frame, dsize=(0, 0), fx=1/2, fy=1/2)
-    cv2.namedWindow("frame")
-    cv2.setMouseCallback("frame", plot_coords)
+# read first frame
+ret, frame = dyad_cap.read()
 
-    while True:
-        # display the  frame
-        cv2.imshow('frame', frame)
-        key = cv2.waitKey(1) & 0xFF
+frame = cv2.resize(frame, dsize=(0, 0), fx=1/2, fy=1/2)
+cv2.namedWindow("frame")
+cv2.setMouseCallback("frame", plot_coords)
 
-        if key == ord("q"):
-            break
+while True:
+    # display the  frame
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(1) & 0xFF
 
-    cv2.waitKey(0)
-    dyad_cap.release()
-    cv2.destroyAllWindows()
+    if key == ord("q"):
+        break
 
-
-def main():
-    input_reference()
-
-if __name__ == "__main__":
-    main()
+cv2.waitKey(0)
+dyad_cap.release()
+cv2.destroyAllWindows()
