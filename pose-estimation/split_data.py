7b5d86a1d4b1878bc47d5a9ec200fcfaa3bbaf7e
Adrien Gregorj <adrien.gregorj@gmail.com>
2018-04-17 14:05:33 +0900

    add script to split data

--- pose-estimation/split_data.py
+++ pose-estimation/split_data.py
@@ -1,16 +1,37 @@
 import os
 from pathlib import Path
+from os import listdir
+import random
+from shutil import copyfile
 
 
 if __name__ == "__main__":
     # folder that contains the st-gcn files (one per video)
     stgcn_json_path =  '../data/poses/normalized_st-gcn_format/'
+    training_path =  '../data/st-gcn/training/'
+    test_path = '../data/st-gcn/test/'
 
     train_ratio = 0.6
 
-    p = Path(stgcn_json_path)
-    
-    for path in p.glob('*.json'):
-        json_path = str(path)
+    dataset = [stgcn_json_path + f for f in listdir(stgcn_json_path)]
+
+    n = len(dataset)
+    n_train = round(train_ratio * n)
+    shuffled_set = random.sample(dataset, n)
+    train_set = shuffled_set[:n_train]
+    test_set = shuffled_set[n_train:]
+
+    for path in train_set:
+        file_name = path.split('/')[-1]
+        dest_path = training_path + file_name
+        print('Copying {} to {}'.format(path, dest_path))
+        copyfile(path, dest_path)
+
+    for path in test_set:
+        file_name = path.split('/')[-1]
+        dest_path = test_path + file_name
+        print('Copying {} to {}'.format(path, dest_path))
+        copyfile(path, dest_path)
+
 
             
